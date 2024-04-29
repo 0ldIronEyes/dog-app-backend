efe39d9f055179209eb3481f598adf6268635b65
0ldIronEyes <running.to.mars@gmail.com>
2024-04-29 16:44:13 -0500

    cors1

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://dog-breed-app-omega.vercel.app', // Allow requests from this origin
+  methods: 'GET,POST', // Allow only specific HTTP methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow only specific headers
+  credentials: true, // Allow cookies and authorization headers to be sent
+}));
 
 const DOGBREEDDB_API_KEY = "f6279f4ddbmsh069f02333435c3ap151352jsncced84bc0811"
 const PETFINDER_API_KEY = "FyGKpuRKqsV5w5M8A3VzaeuL51yB05eIuNKByVDsM526hPD99X";
