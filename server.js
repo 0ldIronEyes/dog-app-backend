75b4d2d799b8aa02aec0469f30d2fa3b08b851b5
0ldIronEyes <running.to.mars@gmail.com>
2024-04-29 13:57:43 -0500

    fixed server.js import error

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const axios = require('axios');
-const { default: DogBreedApi } = import('../src/api.js');
 const PORT = process.env.PORT || 5000; 
 
 
