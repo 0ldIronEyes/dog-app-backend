b454b48ba62e07196741cbb58c48fdf047fd7b1a
0ldIronEyes <running.to.mars@gmail.com>
2024-06-07 08:24:13 -0500

    split server.js

---
+++ helpers/petfinderHelper.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+
+
+const DOGBREEDDB_API_KEY = "f6279f4ddbmsh069f02333435c3ap151352jsncced84bc0811"
+const PETFINDER_API_KEY = "FyGKpuRKqsV5w5M8A3VzaeuL51yB05eIuNKByVDsM526hPD99X";
+const SECRET = "kkmUznOGbMAyVPOeoTmgTDJqllXi43MWEPNg3Mvl";
+
+let petfinderAccessToken = null;
+let petfinderTokenExpiration = null
+
+const getPetfinderAccessToken = async () => {
+  try {
+    const response = await axios.post('https://api.petfinder.com/v2/oauth2/token', {
+      grant_type: 'client_credentials',
+      client_id: PETFINDER_API_KEY,
+      client_secret: SECRET,
+    });
+
+    petfinderAccessToken = response.data.access_token;
+    petfinderTokenExpiration = Date.now() + response.data.expires_in * 1000;
+  } catch (error) {
+    console.error('Error fetching Petfinder access token:', error);
+  }
+};
+
+
+const isTokenExpired = () => {
+  return petfinderTokenExpiration && Date.now() >= petfinderTokenExpiration;
+};
+
+const refreshTokenIfNeeded = async () => {
+  if (isTokenExpired()) {
+  
+    await getPetfinderAccessToken();
+  }
+};
+
+const checkAccessToken = async (req, res, next) => {
+  try {
+    await refreshTokenIfNeeded();
+    if (!petfinderAccessToken) {
+      return res.status(500).json({ error: 'Petfinder access token not available' });
+    }
+    next();
+  } catch (error) {
+    console.error('Error checking access token:', error);
+    res.status(500).json({ error: 'An error occurred while checking access token' });
+  }
+};
+
+module.exports = {
+    getPetfinderAccessToken,
+    isTokenExpired,
+    refreshTokenIfNeeded,
+    checkAccessToken,
+    DOGBREEDDB_API_KEY,
+    PETFINDER_API_KEY,
+    SECRET,
+    petfinderAccessToken,
+    petfinderTokenExpiration
+}
---
+++ routes/users.js
@@ -0,0 +1,139 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+const axios = require('axios');
+const express = require("express");
+const { BadRequestError } = require("../expressError");
+const {User} = require("../models.js");
+const { createToken } = require("../helpers/tokens");
+const userAuthSchema = require("../userAuth.json");
+const userUpdateSchema = require("../userUpdate.json");
+const userRegisterSchema = require("../userRegister.json");
+
+const router = express.Router();
+
+
+
+function ensureCorrectUser(req, res, next) {
+    try {
+      const user = res.locals.user;
+      if (!(user && (user.username === req.params.username))) {
+       throw new UnauthorizedError();
+      }
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  }
+  
+
+  router.post("/token", async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userAuthSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+  
+      const { username, password } = req.body;
+      const user = await User.authenticate(username, password);
+      const token = createToken(user);
+      return res.json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
+  
+   router.post("/register", async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userRegisterSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+  
+      const newUser = await User.register({ ...req.body });
+      const token = createToken(newUser);
+      return res.status(201).json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
+  
+  
+  // GET /[username] => { user }
+  
+  router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const user = await User.get(req.params.username);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
+  
+  /** PATCH /[username] { user } => { user }
+   *
+   * Data can include:
+   *   { firstName, lastName, password, email }
+   *
+   * Returns { username, firstName, lastName, email }
+   
+   * Authorization required: admin or same-user-as-:username
+   **/
+  
+  router.patch("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userUpdateSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+      console.log(req.body);
+      try {
+        await User.authenticate(req.params.username, req.body.password);
+        } catch (err) {
+        if (err instanceof UnauthorizedError) {
+          return next(err); 
+        }
+        throw err; 
+      }
+      const user = await User.update(req.params.username, req.body);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
+  
+  router.post("/:username/favorites/add/:breedname", async function (req, res, next) {
+    try {
+  
+      const height = req.query.height;
+      const weight = req.query.weight;
+      const life = req.query.life;
+      const {username, breedname} = req.params;
+      await User.addFavoriteBreed(username, breedname, life, weight, height)
+      return res.json({ favorited: breedname });
+    } catch (err) {
+      return next(err);
+    }
+  });
+  
+  router.post("/:username/favorites/remove/:breedname", async function (req, res, next) {
+    try {
+  
+      const {username, breedname} = req.params;
+      await User.removeFavoriteBreed(username, breedname)
+      return res.json({ unfavorited: breedname });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  module.exports = router;
